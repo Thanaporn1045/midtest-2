1937d9fe23094735c9edad0bfb0d5b6b1e98bf60
Thanaporn1045 <78547225+Thanaporn1045@users.noreply.github.com>
2021-03-28 21:44:53 +0700

    Add files via upload
    ข้อสอง ครน

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace สอบมิดข้อสอง_ครน
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b,x,y;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+            if (x == y)
+                Console.WriteLine(x);
+            else
+            {
+                do
+                {
+                    if (x < y)
+                    {
+                        x = x + a;
+                    }
+                    else
+                    {
+                        y = y + b;
+                    }
+                }
+                while (x != y);
+                Console.WriteLine(x);
+            }
+            
+
+        }
+    }
+}
